e862812a42ad87c073873502c4a1337354244bdb
yeseong33 <ohyeesung@naver.com>
2024-09-09 10:20:44 +0900

    feat: webconfig file.

---
+++ src/main/java/com/cockatoo/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.cockatoo.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("http://localhost:5173")  // 허용할 도메인 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
+                .allowedHeaders("*")  // 허용할 헤더
+                .allowCredentials(true);  // 쿠키 허용 여부
+    }
+}
